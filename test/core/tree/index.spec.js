404c1f02b00f2bc8f60b0135629cd274e4c4ed0e
e-cloud <saintscott119@gmail.com>
2016-08-25 21:25:54 +0800

    test: set up part of basic tests, test the registry for extensibility

---
+++ test/core/tree/index.spec.js
@@ -0,0 +1,67 @@
+import $ from 'jquery'
+import {expect} from 'chai'
+import treeModule from '../../../src/TreeModule'
+import Tree, {registry as treeRegistry} from '../../../src/core/tree'
+
+describe('Tests for core Tree', function () {
+    before('', function () {
+
+    })
+
+    after('', function () {
+
+    })
+
+    beforeEach('', function () {
+
+    })
+
+    afterEach('', function () {
+
+    })
+
+    describe('test registry', function () {
+        afterEach(function () {
+            treeRegistry.update(Tree)
+            $('#tree-test-1').remove()
+        })
+
+        it('should export the Tree class', function () {
+            const elem = $('<ul id="tree-test-1"></ul>').appendTo('body')
+
+            const treeInstance = treeModule.init(elem, {}, [])
+
+            expect(treeInstance).to.be.an.instanceOf(Tree)
+        })
+
+        it('should be able to override the exported constructor', function () {
+            const testId = 'abcdefg'
+
+            class TestTree extends treeRegistry.constructor {
+                constructor(...args) {
+                    super(...args)
+                    this.testId = testId
+                }
+            }
+
+            treeRegistry.update(TestTree)
+
+            const elem = $('<ul id="tree-test-1"></ul>').appendTo('body')
+
+            const treeInstance = treeModule.init(elem, {}, [])
+
+            expect(treeInstance).to.be.an.instanceOf(TestTree)
+            expect(treeInstance.testId).to.be.equal(testId)
+        })
+
+        it('should throw error when updating constructor via direct assignment', function() {
+            function test(){
+                treeRegistry.constructor = Tree
+            }
+
+            expect(test).to.throw(Error)
+        })
+    })
+
+
+})
