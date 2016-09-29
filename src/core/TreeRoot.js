70a8025e3df010978360ca54c819487710330496
e-cloud <saintscott119@gmail.com>
2016-09-29 19:18:09 +0800

    feat: add basic TreeRoot and its spec

--- src/core/TreeRoot.js
+++ src/core/TreeRoot.js
@@ -1,7 +1,37 @@
-class TreeRoot {
-    constructor() {}
+import TreeNode from './treeNode/TreeNode'
+
+class TreeRoot extends TreeNode {
+    constructor(treeId) {
+        super(generateNodeId(treeId, 0))
+
+        this.treeId = treeId
+        this.nIdCache = this.nId = 0
+
+        this.setParent()
+    }
 
     generateNodeId() {
+        /* eslint no-plusplus:0 */
+        this.nIdCache++
+        return generateNodeId(this.treeId, this.nIdCache)
+    }
 
+    setParent() {
+        this.parent = null
+        this.level = -1
+    }
+
+    remove() {
+        throw Error(`Can't not remove virtual root: ${this.treeId}`)
+    }
+
+    toString() {
+        return `This is virtual root of tree [${this.treeId}].`
     }
 }
+
+function generateNodeId(treeId, nId) {
+    return `${treeId}_${nId}`
+}
+
+export default TreeRoot
---
+++ test/core/TreeRoot.spec.js
@@ -0,0 +1,7 @@
+import TreeNode from '../../src/core/TreeRoot'
+
+describe('Tests for core TreeRoot', function () {
+    it('should initiate an instance of TreeRoot', function () {
+
+    })
+})
